7f25fe6aa86ae4b47db4d91fb2e511334bed84d4
Ivan Mykhavko <7047662+tegos@users.noreply.github.com>
2026-06-28 10:01:10 +0300

    refactor: convert routes to async/await, add /json error handling (#16)

--- routes/index.js
+++ routes/index.js
@@ -9,54 +9,48 @@ router.get('/about', function (req, res, next) {
 });
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    var view_data = {title: 'LvivTransportMonitoringExpress'};
+router.get('/', async function (req, res, next) {
+    const view_data = {title: 'LvivTransportMonitoringExpress'};
 
-    var buses = model.getBuses();
+    try {
+        const response = await model.getBuses();
+        const content = response.getBody();
+        const stops = JSON.parse(content);
 
-    buses.then(function (response) {
-            var content = response.getBody();
-            var stops = JSON.parse(content);
+        stops.forEach(function (stop, key) {
+            const words = stop['Name'].split(" ");
+            stop['SmallName'] = words[0];
+            stops[key] = stop;
+        });
 
-            stops.forEach(function (stop, key) {
-                var words = stop['Name'].split(" ");
-                stop['SmallName'] = words[0];
-                stops[key] = stop;
-            });
+        view_data.stops = stops;
 
-            view_data.stops = stops;
-
-            return res.render('pages/index',
-                view_data
-            );
-        }, function (error) {
-            let errorMessage = "Request Failed";
-            if (error.code && error.body) {
-                errorMessage += " - " + error.code + ": " + error.body
-            }
-
-            console.log(error);
-            return res.status(400).send({
-                message: errorMessage
-            });
+        return res.render('pages/index', view_data);
+    } catch (error) {
+        let errorMessage = "Request Failed";
+        if (error.code && error.body) {
+            errorMessage += " - " + error.code + ": " + error.body;
         }
-    );
 
+        console.log(error);
+        return res.status(400).send({
+            message: errorMessage
+        });
+    }
 });
 
 /* GET Json Data */
-router.get('/json', function (req, res, next) {
-    console.log('get json');
-
-    var buses = model.getBuses();
-
-    buses.then(function (response) {
-            var content = response.getBody();
-            var json = JSON.parse(content);
-            res.send(json);
-        }
-    );
-
+router.get('/json', async function (req, res, next) {
+    try {
+        const response = await model.getBuses();
+        const content = response.getBody();
+        const json = JSON.parse(content);
+        res.send(json);
+    } catch (error) {
+        return res.status(500).send({
+            message: error.message || "Internal Server Error"
+        });
+    }
 });
 
 module.exports = router;
